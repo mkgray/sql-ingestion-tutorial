3e5a1726e8a81811277c14f02ae348c550e866e8
Matt Gray <drmkgray@gmail.com>
2021-10-17 18:55:53 -0400

    Cleaned up pyspark script

--- scripts/pyspark_connection.py
+++ scripts/pyspark_connection.py
@@ -1,9 +1,18 @@
+# Set configuration for DB
+MYSQL_HOST="mysql-server"
+MYSQL_USER="remote_connect_user"
+MYSQL_PASS="remote_connect_password"
+MYSQL_DB="test_db"
+MYSQL_TABLE="test_table"
+
+# Establish connection for MySQL DB using credentials provided
 df = spark.read.format('jdbc').options(
-    url = "jdbc:mysql://mysql-server/test_db",
+    url = f"jdbc:mysql://{MYSQL_HOST}/{MYSQL_DB}",
     driver = "com.mysql.cj.jdbc.Driver",
-    dbtable = "test_table",
-    user = "remote_connect_user",
-    password = "remote_connect_password"
+    dbtable = MYSQL_TABLE,
+    user = MYSQL_USER,
+    password = MYSQL_PASS
 ).load()
 
+# Display results for validating successful connection
 df.show()
