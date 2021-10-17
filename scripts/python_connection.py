49eb67fa5f9932265bba36341ec11d9094b4956f
Matt Gray <drmkgray@gmail.com>
2021-10-17 19:00:04 -0400

    Cleaned up python script

--- scripts/python_connection.py
+++ scripts/python_connection.py
@@ -1,12 +1,19 @@
+# Imports
 import pyodbc
-server = 'mysql-server'
-database = 'test_db' 
-username = 'remote_connect_user' 
-password = 'remote_connect_password' 
-cnxn = pyodbc.connect('DRIVER={MySQL ODBC 8.0 Driver};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+
+# Set configuration for DB
+MYSQL_HOST="mysql-server"
+MYSQL_USER="remote_connect_user"
+MYSQL_PASS="remote_connect_password"
+MYSQL_DB="test_db"
+MYSQL_TABLE="test_table"
+
+# Establish connection to MySQL DB
+cnxn = pyodbc.connect(f"DRIVER={{MySQL ODBC 8.0 Driver}};SERVER={MYSQL_HOST};DATABASE={MYSQL_DB};UID={MYSQL_USER};PWD={MYSQL_PASS}")
 cursor = cnxn.cursor()
 
-cursor.execute("SELECT * FROM test_table")
+# Run query to select * and output results
+cursor.execute(f"SELECT * FROM {MYSQL_TABLE}")
 row = cursor.fetchone()
 while row:
     print (row)
